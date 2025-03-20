d697ec3fd78cc8bd7d324144029afc1e3048239a
Joel Lacour <silverclaw@noreply.codeberg.org>
2025-03-20 22:34:02 +0100

    add wgpu-native as lazy dependency

--- build.zig
+++ build.zig
@@ -5,9 +5,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("webgpu", .{
+    var module = b.addModule("webgpu", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const wgpu = b.lazyDependency("wgpu_native", .{}).?;
+    module.addObjectFile(wgpu.path("lib/libwgpu_native.a"));
 }
