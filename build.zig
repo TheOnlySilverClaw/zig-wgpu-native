fa277cf1e894269024b1ea76bf5be22019eab3c4
Joel Lacour <silverclaw@noreply.codeberg.org>
2025-03-22 10:22:48 +0100

    remove linked library

--- build.zig
+++ build.zig
@@ -5,12 +5,9 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    var module = b.addModule("webgpu", .{
+    _ = b.addModule("webgpu", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const wgpu = b.lazyDependency("wgpu_native", .{}).?;
-    module.addObjectFile(wgpu.path("lib/libwgpu_native.a"));
 }
