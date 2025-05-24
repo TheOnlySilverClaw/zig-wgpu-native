6f6ede989ece7af5980dff600e6dbd4f6870f2a2
Joel Lacour <silverclaw@noreply.codeberg.org>
2025-05-24 16:25:03 +0200

    add VertexFormat byte sizes

--- src/render_pipeline.zig
+++ src/render_pipeline.zig
@@ -134,7 +134,53 @@ pub const VertexFormat = enum(u32) {
     sint32x3,
     sint32x4,
     unorm_10_10_2,
-    unorm_8x4_bgra
+    unorm_8x4_bgra,
+
+    pub fn size(format: webgpu.VertexFormat) u32 {
+        return switch (format) {
+            .uint8 => @sizeOf(u8),
+            .uint8x2 => @sizeOf(u8) * 2,
+            .uint8x4 => @sizeOf(u8) * 4,
+            .sint8 => @sizeOf(i8),
+            .sint8x2 => @sizeOf(i8) * 2,
+            .sint8x4 => @sizeOf(i8) * 2,
+            .unorm8 => @sizeOf(u8),
+            .unorm8x2 => @sizeOf(u8) * 2,
+            .unorm8x4 => @sizeOf(u8) * 4,
+            .snorm8 => @sizeOf(i8),
+            .snorm8x2 => @sizeOf(i8) * 2,
+            .snorm8x4 => @sizeOf(i8) * 2,
+            .uint16 => @sizeOf(u16),
+            .uint16x2 => @sizeOf(u16) * 2,
+            .uint16x4 => @sizeOf(u16) * 4,
+            .sint16 => @sizeOf(i16),
+            .sint16x2 => @sizeOf(i16) * 2,
+            .sint16x4 => @sizeOf(i16) * 4,
+            .unorm16 => @sizeOf(u16),
+            .unorm16x2 => @sizeOf(u16) * 2,
+            .unorm16x4 => @sizeOf(u16) * 4,
+            .snorm16x2 => @sizeOf(i16) * 2,
+            .snorm16 => @sizeOf(i16),
+            .snorm16x4 => @sizeOf(i16) * 4,
+            .float16 => @sizeOf(f16),
+            .float16x2 => @sizeOf(f16) * 2,
+            .float16x4 => @sizeOf(f16) * 4,
+            .float32 => @sizeOf(f32),
+            .float32x2 => @sizeOf(f32) * 2,
+            .float32x3 => @sizeOf(f32) * 3,
+            .float32x4 => @sizeOf(f32) * 4,
+            .uint32 => @sizeOf(u32),
+            .uint32x2 => @sizeOf(u32) * 2,
+            .uint32x3 => @sizeOf(u32) * 3,
+            .uint32x4 => @sizeOf(u32) * 4,
+            .sint32 => @sizeOf(i32),
+            .sint32x2 => @sizeOf(i32) * 2,
+            .sint32x3 => @sizeOf(i32) * 3,
+            .sint32x4 => @sizeOf(i32) * 4,
+            .unorm_10_10_2 => 10 + 10 + 2,
+            .unorm_8x4_bgra => 8 * 4
+        };
+    }
 };
 
 pub const VertexStepMode = enum(u32) {
